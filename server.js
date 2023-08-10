bfb2deefd5f558362b7272a0291d78c250b9c2a6
SauravNaik04 <104769411+SauravNaik04@users.noreply.github.com>
2023-08-10 14:17:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 //configure env
