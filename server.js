8b5898bb0a8bcd027fa6a62b9b4c9a38bdd424e3
SauravNaik04 <104769411+SauravNaik04@users.noreply.github.com>
2023-08-10 13:08:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -16,8 +16,8 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
-const __filename = fileURLToPath(import.meta.URL);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.URL);
+// const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
